74f4a1ea5934d31202ecc0b1af166a31aabe5672
Michael Astrauckas <mastrauckas@hotmail.com>
2017-07-04 09:50:34 -0400

    Adding dotnet linux 2.0.0 preview 2 version.  This image is also hosted on docker.

---
+++ src/dotnet-2.0.0-preview2-linux/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_dotnet_core
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Hello from dotnet core:");
+            Console.WriteLine($"      Platform: {System.Runtime.InteropServices.RuntimeInformation.OSDescription}");
+            Console.WriteLine($"      Framework: {Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.RuntimeFramework.Version}");
+        }
+    }
+}
